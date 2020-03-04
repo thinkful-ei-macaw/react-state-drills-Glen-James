3389ec5965561b85cafd2ddc48681ab52a4096b1
Alex Larrabee <nekotree556@gmail.com>
2020-03-04 11:10:07 -0800

    updated

--- src/index.js
+++ src/index.js
@@ -4,9 +4,10 @@ import './index.css';
 import App from './App';
 import HelloWorld from './state-drills/HelloWorld';
 import Bomb from './state-drills/Bomb';
+import RouletteGun from './state-drills/RouletteGun';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Bomb />, document.getElementById('root'));
+ReactDOM.render(<RouletteGun />, document.getElementById('root'));
 
 
 serviceWorker.unregister();
---
+++ src/state-drills/RouletteGun.js
@@ -0,0 +1,27 @@
+import { Component } from "react";
+
+
+
+class RouletteGun extends Component {
+
+    state = {
+
+
+    }
+
+    render() {
+
+        return (
+
+
+
+        )
+
+
+    }
+
+
+
+}
+
+export default RouletteGun
